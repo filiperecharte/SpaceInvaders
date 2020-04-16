26d0d22081ca608aeec2603bb5c21a1bf575e378
filipeeteixeira <filipeteixeira00@hotmail.com>
2020-04-16 20:10:26 +0100

    Ship done

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,9 @@
+import Model.Position;
 import Model.Ship;
 
 public class Application {
     public static void main(String[] args) {
-        Ship s = new Ship(0,0);
+        Ship s = new Ship(new Position(0,0) );
         System.out.println("Hello World!");
         System.out.println(s.getDesign());
     }
