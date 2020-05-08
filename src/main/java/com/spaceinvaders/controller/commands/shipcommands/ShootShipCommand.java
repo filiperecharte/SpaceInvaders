e74f1a893ad299c9468c341f98402905bd79b170
up201806728@ms.uporto.pt <up201806728@ms.uporto.pt>
2020-05-08 18:29:54 +0100

    Comment ShootShipCommand

--- src/main/java/com/spaceinvaders/controller/commands/shipcommands/ShootShipCommand.java
+++ src/main/java/com/spaceinvaders/controller/commands/shipcommands/ShootShipCommand.java
@@ -2,6 +2,6 @@ package com.spaceinvaders.controller.commands.shipcommands;
 
 import com.spaceinvaders.controller.commands.Command;
 
-public class ShootShipCommand implements Command {
+/*public class ShootShipCommand implements Command {
 
-}
+}*/
