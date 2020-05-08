8eb56c3dd43cc01b903126eb835da7afa04486f4
up201806728@ms.uporto.pt <up201806728@ms.uporto.pt>
2020-05-08 17:57:47 +0100

    Add speed property to Shot

--- src/main/java/com/spaceinvaders/model/shots/Shot.java
+++ src/main/java/com/spaceinvaders/model/shots/Shot.java
@@ -6,15 +6,20 @@ import com.spaceinvaders.model.geometry.Size;
 import com.spaceinvaders.model.image.Image;
 
 public class Shot extends Frame {
-    protected int damage;
+    protected int damage, speed;
 
-    public Shot(Position position, Size size, Image image, int damage) {
+    public Shot(Position position, Size size, Image image, int damage, int speed) {
         super(position, size, image);
         this.damage = damage;
+        this.speed = speed;
     }
 
     public int getDamage() {
         return damage;
+    }
+
+    public int getSpeed() {
+        return speed;
     }
 
     public void update() {
