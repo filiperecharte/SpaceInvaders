8adc76c22f9b38452950dee6bdef711b0b2b8447
up201806728@ms.uporto.pt <up201806728@ms.uporto.pt>
2020-05-10 12:00:37 +0100

    Create Ship tests and fix getShootPosition method in Ship

--- src/main/java/com/spaceinvaders/model/ship/Ship.java
+++ src/main/java/com/spaceinvaders/model/ship/Ship.java
@@ -4,6 +4,8 @@ import com.spaceinvaders.model.MovableFrame;
 import com.spaceinvaders.model.Shooter;
 import com.spaceinvaders.model.geometry.Position;
 import com.spaceinvaders.model.geometry.Size;
+import com.spaceinvaders.model.geometry.Translaction;
+import com.spaceinvaders.model.geometry.Vector;
 import com.spaceinvaders.model.image.Image;
 
 public class Ship extends MovableFrame implements Shooter {
@@ -13,10 +15,14 @@ public class Ship extends MovableFrame implements Shooter {
 
         if (!this.isPerfectlyOverlapped())
             throw new IllegalArgumentException("Image must perfectly overlap Box");
+
+        if (size.getWidth() % 2 == 0)
+            throw new IllegalArgumentException("Ship size must be odd");
     }
 
     @Override
     public Position getShootPosition() {
-        return null;
+        Translaction t = new Translaction(this.position, new Vector((this.size.getWidth() - 1) / 2, 0));
+        return t.apply();
     }
 }
