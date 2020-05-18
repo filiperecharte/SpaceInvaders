290317a8ef4742c3885ca24522d706f0e3adcd17
up201806728@ms.uporto.pt <up201806728@ms.uporto.pt>
2020-05-18 22:10:17 +0100

    Solve conflits in Fragment

--- src/main/java/com/spaceinvaders/model/wall/Fragment.java
+++ src/main/java/com/spaceinvaders/model/wall/Fragment.java
@@ -1,31 +1,22 @@
 package com.spaceinvaders.model.wall;
 
-import com.spaceinvaders.exceptions.IllegalArgumentException;
 import com.spaceinvaders.model.Box;
 import com.spaceinvaders.model.collisions.ICollideableVisited;
 import com.spaceinvaders.model.collisions.ICollideableVisitor;
 import com.spaceinvaders.model.geometry.Position;
 import com.spaceinvaders.model.geometry.Size;
-import com.spaceinvaders.model.image.Image;
-import com.spaceinvaders.model.image.Pixel;
 
 public class Fragment extends Box implements ICollideableVisited {
     private char c;
-    private Image image;
 
-    public Fragment(Position position, Size size, Image image, char c) {
+    public Fragment(Position position, Size size, char c) {
         super(position, size);
         this.c=c;
-        this.image= image;
 
     }
 
     public char getChar(){
         return c;
-    }
-
-    public Image getImage() {
-        return image;
     }
 
     @Override
