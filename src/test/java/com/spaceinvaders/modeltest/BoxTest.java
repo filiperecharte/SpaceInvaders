68b51c4f12e6a97ce372d7566ebad286660f001d
up201806728@ms.uporto.pt <up201806728@ms.uporto.pt>
2020-05-19 14:10:07 +0100

    Test contain box

--- src/test/java/com/spaceinvaders/modeltest/BoxTest.java
+++ src/test/java/com/spaceinvaders/modeltest/BoxTest.java
@@ -12,11 +12,20 @@ import static org.junit.Assert.assertTrue;
 public class BoxTest {
 
     @Test
-    public void CointainsTest() {
+    public void containsElementTest() {
         Box box = new Box(new Position(10, 20), new Size(20, 20));
 
         assertFalse(box.contain(new Position(5,5)));
         assertFalse(box.contain(new Position(30,52)));
         assertTrue(box.contain(new Position(16,30)));
+    }
+
+    @Test
+    public void containsBoxTest() {
+        Box box = new Box(new Position(100, 100), new Size(100, 70));
+
+        assertFalse(box.contain(new Box(new Position(50, 50), new Size(100, 50))));
+        assertFalse(box.contain(new Box(new Position(50, 50), new Size(200, 200))));
+        assertTrue(box.contain(new Box(new Position(120, 120), new Size(50, 50))));
     }
 }
