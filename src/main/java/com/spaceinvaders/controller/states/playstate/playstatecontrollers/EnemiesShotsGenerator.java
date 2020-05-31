966b535a0455801708b696173a2a850c49c8aec9
up201806728@ms.uporto.pt <up201806728@ms.uporto.pt>
2020-05-31 09:34:40 +0100

    Finish ShotsController tests

---
+++ src/main/java/com/spaceinvaders/controller/states/playstate/playstatecontrollers/EnemiesShotsGenerator.java
@@ -0,0 +1,36 @@
+package com.spaceinvaders.controller.states.playstate.playstatecontrollers;
+
+import com.spaceinvaders.model.arena.Arena;
+import com.spaceinvaders.model.enemy.Enemy;
+import com.spaceinvaders.model.pools.ShotPool;
+import com.spaceinvaders.model.shots.Shot;
+
+public class EnemiesShotsGenerator {
+    private Arena arena;
+    private ShotPool shotPool;
+    private int whenToShoot;
+    private Enemy enemyToShoot;
+
+    public EnemiesShotsGenerator(Arena arena, ShotPool shotPool) {
+        this.arena = arena;
+        this.shotPool = shotPool;
+    }
+
+    public void generate() {
+        Shot shot = shotPool.extract();
+        enemyToShoot.processShot(shot);
+        arena.addElement(shot);
+    }
+
+    public boolean enemyReadyToShoot() {
+        return !arena.getEnemies().isEmpty() && enemyToShoot.getAttackBehavior().readyToShoot(whenToShoot);
+    }
+
+    public void setWhenToShoot(int whenToShoot) {
+        this.whenToShoot = whenToShoot;
+    }
+
+    public void setEnemyIndex(int enemyIndex) {
+        this.enemyToShoot = arena.getEnemies().get(enemyIndex);
+    }
+}
--- src/test/java/com/spaceinvaders/controllertest/ShotsControllerTest.java
+++ src/test/java/com/spaceinvaders/controllertest/ShotsControllerTest.java
@@ -1,75 +1,110 @@
 package com.spaceinvaders.controllertest;
 
 import com.spaceinvaders.controller.states.playstate.playstatecontrollers.ShotsController;
-import org.junit.Before;
+import com.spaceinvaders.exceptions.IllegalArgumentException;
+import com.spaceinvaders.model.arena.Arena;
+import com.spaceinvaders.model.behaviors.AttackBehavior;
+import com.spaceinvaders.model.behaviors.DamageBehavior;
+import com.spaceinvaders.model.behaviors.HealthyBehavior;
+import com.spaceinvaders.model.behaviors.MovableBehavior;
+import com.spaceinvaders.model.enemy.Enemy;
+import com.spaceinvaders.model.geometry.Position;
+import com.spaceinvaders.model.geometry.Size;
+import com.spaceinvaders.model.geometry.Vector;
+import com.spaceinvaders.model.pools.ShotPool;
+import com.spaceinvaders.model.ship.Ship;
+import com.spaceinvaders.model.shots.Shot;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ShotsControllerTest {
-    ShotsController shotsController;
 
-    @Before
-    public void setUp() {
-        /*Arena arena = new Arena(new Position(0, 0), new Size(10, 10), "#000000");
+    @Test
+    public void processShotsTest1() {
+        Arena arena = new Arena(new Position(0, 0), new Size(10, 10), "");
         try {
-            arena.addElement(new WeakShot());
-            arena.addElement(new YoungEnemy(new Position(3, 2), new Size(2, 1)));
-            arena.addElement(new WiseEnemy(new Position(5, 2), new Size(2, 1)));
-            arena.addElement(new StrongEnemy(new Position(7, 2), new Size(2, 1)));
-            arena.addElement(new SuperiorEnemy(new Position(9, 2), new Size(2, 1)));
+            arena.addElement(new Ship(new Position(0, 0), new Size(5, 1), new HealthyBehavior(1)));
 
-            Wall wall = new Wall(new Position(5, 5));
-            wall.addFragment(new Fragment(new Position(5, 5), new Size(1, 1)));
-            arena.addElement(wall);
+            Shot shot = new Shot(new Position(10, 10), new Size(1, 1));
+            shot.setDamageBehavior(new DamageBehavior(1));
+            shot.setMovableBehavior(new MovableBehavior(new Vector(0, -1)));
+            arena.addElement(shot);
 
-            //arena.addElement(new Ship(new Position 8, 8));
         } catch (IllegalArgumentException e) { e.printStackTrace(); }
 
-        ShotPool shotPool = new ShotPool();
-
-        shotsController = new ShotsController(arena, shotPool);*/
-    }
-
-    /*@Test
-    public void processShotsTest() {
-
-        ShotsController shotsControllerSpy = Mockito.spy(shotsController);
-        doNothing().when(shotsControllerSpy).updateShot(Mockito.any());
-        doNothing().when(shotsControllerSpy).checkShotCollision(Mockito.any(), Mockito.any());
-        doNothing().when(shotsControllerSpy).shotToPoolGroup(Mockito.any(),Mockito.any());
-
-        shotsControllerSpy.processShots();
-
-        verify(mockArena,times(mockArena.getShots().size())).contain((Position)Mockito.any());
-
+        assertEquals(1, arena.getShots().size());
+        ShotsController shotsController = new ShotsController(arena, new ShotPool());
+        shotsController.processShots();
+        assertEquals(0, arena.getShots().size());
     }
 
     @Test
-    public void updateShotTest() {
-        Shot testShot = null;
+    public void processShotsTest2() {
+        Arena arena = new Arena(new Position(0, 0), new Size(10, 10), "");
         try {
-            testShot = new Shot(new Position(15, 20), new Size(3, 1), new MovableBehavior(new Vector(1,0)), new DamageBehavior(10));
-        } catch(IllegalArgumentException e){
-            e.printStackTrace();
-        }
-        assert testShot != null;
+            arena.addElement(new Ship(new Position(0, 0), new Size(5, 1), new HealthyBehavior(1)));
 
-        shotsController.updateShot(testShot);
+            Shot shot = new Shot(new Position(5, 5), new Size(1, 1));
+            shot.setDamageBehavior(new DamageBehavior(1));
+            shot.setMovableBehavior(new MovableBehavior(new Vector(0, -1)));
+            arena.addElement(shot);
 
-        assertEquals(16, testShot.getPosition().getX());
-        assertEquals(20, testShot.getPosition().getY());
+        } catch (IllegalArgumentException e) { e.printStackTrace(); }
 
+        assertEquals(1, arena.getShots().size());
+        ShotsController shotsController = new ShotsController(arena, new ShotPool());
+        shotsController.processShots();
+        assertEquals(1, arena.getShots().size());
     }
 
     @Test
-    public void generateEnemyShotTest() {
-        Random mockRandom = new Random();
-        when(mockRandom.nextInt()).thenReturn(15).thenReturn(3);
+    public void processShotsTest3() {
+        Arena arena = new Arena(new Position(0, 0), new Size(10, 10), "");
+        try {
+            arena.addElement(new Ship(new Position(0, 0), new Size(5, 1), new HealthyBehavior(1)));
 
-        ShotsController shotsControllerSpy = Mockito.spy(shotsController);
-        when(shotsControllerSpy.makeRandom()).thenReturn(mockRandom).thenReturn(mockRandom);
+            Enemy enemy = new Enemy(new Position(6, 6), new Size(3, 1));
+            enemy.setAttackBehavior(new AttackBehavior(10));
+            enemy.setHealthyBehavior(new HealthyBehavior(1));
+            enemy.setMovableBehavior(new MovableBehavior(new Vector(1, 0)));
+            arena.addElement(enemy);
 
-        assertEquals(16, testShot.getPosition().getX());
-        assertEquals(20, testShot.getPosition().getY());
+            Shot shot = new Shot(new Position(6, 6), new Size(1, 1), "ShipShot", "ShipShotImg");
+            shot.setDamageBehavior(new DamageBehavior(1));
+            shot.setMovableBehavior(new MovableBehavior(new Vector(0, -1)));
+            arena.addElement(shot);
 
-    }*/
+        } catch (IllegalArgumentException e) { e.printStackTrace(); }
 
+        assertEquals(1, arena.getShots().size());
+        ShotsController shotsController = new ShotsController(arena, new ShotPool());
+        shotsController.processShots();
+        assertEquals(0, arena.getShots().size());
+    }
+
+    @Test
+    public void processShotsTest4() {
+        Arena arena = new Arena(new Position(0, 0), new Size(10, 10), "");
+        try {
+            arena.addElement(new Ship(new Position(0, 0), new Size(5, 1), new HealthyBehavior(1)));
+
+            Enemy enemy = new Enemy(new Position(6, 6), new Size(3, 1));
+            enemy.setAttackBehavior(new AttackBehavior(10));
+            enemy.setHealthyBehavior(new HealthyBehavior(1));
+            enemy.setMovableBehavior(new MovableBehavior(new Vector(1, 0)));
+            arena.addElement(enemy);
+
+            Shot shot = new Shot(new Position(6, 5), new Size(1, 1), "ShipShot", "ShipShotImg");
+            shot.setDamageBehavior(new DamageBehavior(1));
+            shot.setMovableBehavior(new MovableBehavior(new Vector(0, -1)));
+            arena.addElement(shot);
+
+        } catch (IllegalArgumentException e) { e.printStackTrace(); }
+
+        assertEquals(1, arena.getShots().size());
+        ShotsController shotsController = new ShotsController(arena, new ShotPool());
+        shotsController.processShots();
+        assertEquals(1, arena.getShots().size());
+    }
 }
